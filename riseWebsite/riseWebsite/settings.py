0f09bf999bd092b92ff40ac52362ffbb2b27cbc0
Micheal Myers <michealmyers@Micheals-MacBook-Pro.local>
2019-09-27 21:15:52 -0400

    Set up templating

--- riseWebsite/riseWebsite/settings.py
+++ riseWebsite/riseWebsite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'riseWebsite',
+    'publicDisplay',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'riseWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
