a511bf4e18049e23041bd221b0fc119526f3c3fd
Micheal Myers <michealmyers@Micheals-MacBook-Pro.local>
2019-09-28 05:32:39 -0400

    Removed home and contact pages

--- src/riseSite/finalView/urls.py
+++ src/riseSite/finalView/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home.html', views.contact, name='home'),
+    path('', views.search, name='index'),
     path('base.html', views.base, name='base'),
-    path('contact.html', views.contact, name='contact'),
     path('search.html', views.search, name='search'),
     path('result.html', views.result, name='result'),
 ]
