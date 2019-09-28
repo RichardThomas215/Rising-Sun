5ec42f3cccc2b87e5a689b57eba64e6b4b83ffe4
Naiem Gafar <naiemgafar@Naiems-MacBook-Pro.local>
2019-09-28 04:58:25 -0400

    finally pulling from db

--- src/riseSite/finalView/views.py
+++ src/riseSite/finalView/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import *
+
 
 def index(req):
     return render(req, 'finalView/home.html')
@@ -13,4 +15,7 @@ def contact(req):
     return render(req, 'finalView/contact.html')
 
 def result(req):
-    return render(req, 'finalView/result.html')
+    context_dict={}
+    events = Event.objects.all()
+    context_dict['events']=events
+    return render(req, 'finalView/result.html', context_dict)
