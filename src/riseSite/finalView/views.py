2ba8d8cc5701e916c501b84ee8bc008cf2d82f20
Micheal Myers <michealmyers@Micheals-MacBook-Pro.local>
2019-09-28 00:54:06 -0400

    Implemented base template

--- src/riseSite/finalView/views.py
+++ src/riseSite/finalView/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def index(req):
     return render(req, 'finalView/home.html')
 
+def base(req):
+    return render(req, 'finalView/base.html')
+
 def search(req):
     return render(req, 'finalView/search.html')
 
