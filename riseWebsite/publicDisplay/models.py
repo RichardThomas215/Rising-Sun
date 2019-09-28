4d44d793c35b22086661c3dc074a5e189a5c2e7f
Naiem Gafar <naiemgafar@Naiems-MacBook-Pro.local>
2019-09-27 20:43:47 -0400

    made models for task, scheule, and key players

--- riseWebsite/publicDisplay/models.py
+++ riseWebsite/publicDisplay/models.py
@@ -1,4 +1,29 @@
 from django.db import models
 
 # Create your models here.
-class 
+class Event(models.Model):
+    tasks = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=100)
+    key_player_1 = models.OneToOneField(
+        keyPlayers,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    key_player_2 = models.OneToOneField(
+        keyPlayers,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    key_player_3 = models.OneToOneField(
+        keyPlayers,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    deadline = models.DateField(null = True)
+    notes = models.CharField(max_length=100)
+
+class keyPlayers(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
